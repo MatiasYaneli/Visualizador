b095a5e86eea00b105dad7cb40a35aebcd646a9b
matiasYaneli <matias.yaneli@yahoo.com.mx>
2019-08-26 13:30:19 -0500

    button check
    activar botones

---
+++ src/scripts/mapLoader.js
@@ -0,0 +1,11 @@
+var bounds = [
+    [-74.04728500751165, 40.68392799015035], // Southwest coordinates
+    [-73.91058699000139, 40.87764500765852]  // Northeast coordinates
+];
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v10',
+    center: [-99.701254, 19.294099],
+    zoom: 7.5
+});
